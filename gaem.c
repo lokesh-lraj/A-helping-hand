05a80cad707dd430f9a6d7b7676fa99907b7e491
LOKESH_RAJ <52743591+lokesh-lraj@users.noreply.github.com>
2020-12-19 16:44:09 +0530

    Update gaem.c

--- gaem.c
+++ gaem.c
@@ -6,21 +6,20 @@ int main(){
 	scanf("%d",&t);
 	for(int i=0; i<t; ++i){
 		scanf("%d",&n);
-		int arr[n], bob_w=0, bob_l=0;
+		int var, bob_w=0, bob_l=0;
 		for(int j=0; j<n; ++j){
-			scanf("%d",&arr[j]);
-		}
-		for(int j=0; j<n; ++j){
-			if(arr[j]%5 == 3 || arr[j]%5 == 0)
+			scanf("%d",&var);
+			
+			if(var <= 0);
+			
+			else if(var%5 == 3 || var%5 == 0)
 				++bob_w;
-			else if(arr[j] <= 0)
-			{}
 			else
 				++bob_l;
 		}
 		if(bob_w or bob_l)
 		{
-			printf("Bob wins in the game %d times.",bob_w);
+			printf("Bob wins in the game %d times.\n",bob_w);
 			printf("Bob loses in the game %d times.",bob_l);
 		}
 		else{
