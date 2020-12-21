14b61c7b6db2e2fa08afe50d34b78c6b27e6cb74
LOKESH_RAJ <52743591+lokesh-lraj@users.noreply.github.com>
2020-12-21 16:39:04 +0530

    Update super_reduced_string.c

--- super_reduced_string.c
+++ super_reduced_string.c
@@ -1,1 +1,21 @@
-int main()
+char* superReduced(char *s, int n){
+	char final[n];
+	int indx = -1;
+	for(int i=0; i<n; ++i){
+		if(indx == -1){
+			++indx;
+			final[indx] = s[i];
+		}
+		else if(final[indx] == s[i]){
+			--indx;
+		}
+		else{
+			++indx;
+			final[indx] = s[i];
+		}
+	}
+	if(indx == -1)
+		return 'Empty String'
+	final[++indx] = '\0';
+	return final;
+}
