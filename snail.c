0e4b01938d803357c2de384182750c10739865fe
LOKESH_RAJ <52743591+lokesh-lraj@users.noreply.github.com>
2020-12-30 14:09:46 +0530

    Add files via upload

---
+++ snail.c
@@ -0,0 +1,9 @@
+float findDurationToClimb(float l, float X, float P, float Y, float Q, )
+{
+	float up_in_hour = (X/P);
+	float down_in_hour = (Y/Q);
+
+	float diff = (up_in_hour - down_in_hour);
+	float diff = 1/diff;
+	return diff*l;
+}
