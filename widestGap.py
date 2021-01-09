c1109b12bb14755b477a860deb94c982ee1b24d5
LOKESH_RAJ <52743591+lokesh-lraj@users.noreply.github.com>
2021-01-09 12:46:48 +0530

    Update widestGap.py

--- widestGap.py
+++ widestGap.py
@@ -1,7 +1,8 @@
 def widestGap(n, start, finish):
 	count = len(start)
-	ans = 0
+	ans = start[0]-1
 	for i in range(1, count):
 		if (start[i]-finish[i-1]-1) > ans:
 			ans = (start[i]-finish[i-1]-1)
+	ans = max(ans, n-finish[-1]-1)
 	return ans
