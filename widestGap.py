8a80186657fb9c32603542bf721d90730fd23a88
LOKESH_RAJ <52743591+lokesh-lraj@users.noreply.github.com>
2021-01-09 13:06:13 +0530

    Update widestGap.py

--- widestGap.py
+++ widestGap.py
@@ -1,11 +1,22 @@
 def widestGap(n, start, finish):
 	count = len(start)
-	d = dict(zip(start, finish))
+	d = dict()
+	for i, elem in enumerate(start):
+		if elem in d:
+			d[elem] = max(d[elem], finish[i])
+		else:
+			d[elem] = finish[i]
+
 	d = sorted(d.items())
-	pr = d[0][1]
-	for i in range(1, count):
-		v = (d[i][0]-pr-1)
-		if v > ans:
-			ans = v
-		pr = max(pr, d[i][1])
+
+	pr = None
+	ans = 0
+	for car in d:
+		if pr == None:
+			pr = car[1]
+		else:
+			v = (car[0]-pr-1)
+			if v > ans:
+				ans = v
+			pr = max(pr, car[1])
 	return ans
